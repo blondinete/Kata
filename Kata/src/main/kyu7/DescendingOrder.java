dc28e12934365bb4e30e02b3d85ad3163133666b
mafalda <mafalda.rodrigues.ext@nokia.com>
2019-02-28 14:28:57 +0000

    format files

--- Kata/src/main/kyu7/DescendingOrder.java
+++ Kata/src/main/kyu7/DescendingOrder.java
@@ -5,22 +5,19 @@ import java.util.Arrays;
 public class DescendingOrder {
 
 	public static int sortDesc(final int num) {
-	
-        // solution 1
-        /*return Integer.parseInt(String.valueOf(num)
-                .chars()
-                .mapToObj(i -> String.valueOf(Character.getNumericValue(i)))
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.joining()));*/
-		
+
+		// solution 1
+		/*
+		 * return Integer.parseInt(String.valueOf(num) .chars() .mapToObj(i ->
+		 * String.valueOf(Character.getNumericValue(i)))
+		 * .sorted(Comparator.reverseOrder()) .collect(Collectors.joining()));
+		 */
 
 		// solution 2
-        char[] numArray = String.valueOf(num).toCharArray();
-        Arrays.sort(numArray);		
-        return Integer.parseInt(new StringBuilder(new String(numArray)).reverse().toString());
-        
-        
-        
+		char[] numArray = String.valueOf(num).toCharArray();
+		Arrays.sort(numArray);
+		return Integer.parseInt(new StringBuilder(new String(numArray)).reverse().toString());
+
 	}
 
 }
