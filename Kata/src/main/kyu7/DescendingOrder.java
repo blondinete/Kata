2efbff0622de21d108b52842206283cc99ba9287
mafalda <mafalda.rodrigues.ext@nokia.com>
2019-02-28 14:25:34 +0000

    Add one more solution on Kata Descending Order

--- Kata/src/main/kyu7/DescendingOrder.java
+++ Kata/src/main/kyu7/DescendingOrder.java
@@ -5,13 +5,22 @@ import java.util.Arrays;
 public class DescendingOrder {
 
 	public static int sortDesc(final int num) {
+	
+        // solution 1
+        /*return Integer.parseInt(String.valueOf(num)
+                .chars()
+                .mapToObj(i -> String.valueOf(Character.getNumericValue(i)))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining()));*/
+		
 
-		
-		// solution 1
+		// solution 2
         char[] numArray = String.valueOf(num).toCharArray();
-        Arrays.sort(numArray);
+        Arrays.sort(numArray);		
         return Integer.parseInt(new StringBuilder(new String(numArray)).reverse().toString());
-		
+        
+        
+        
 	}
 
 }
